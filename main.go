085132d83b5492afc3ab6e48ee8fdde54056e52e
adrake333 <adrake333@gmail.com>
2026-07-28 22:02:32 -0500

    working on handler_upload_video

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
@@ -21,7 +22,9 @@ type apiConfig struct {
 	s3Bucket         string
 	s3Region         string
 	s3CfDistribution string
+	s3Client	 *s3.Client
 	port             string
+
 }
 
 type thumbnail struct {
@@ -79,6 +82,12 @@ func main() {
 		log.Fatal("S3_CF_DISTRO environment variable is not set")
 	}
 
+	s3Cfg, err := config.LoadDefaultConfig(context.Background(), config.WithRegion(s3Region))
+	if err != nil {
+		panic(fmt.Sprintf("failed loading config, %v", err))
+	}
+	s3Client := s3.NewFromConfig(s3Cfg)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("PORT environment variable is not set")
@@ -93,6 +102,7 @@ func main() {
 		s3Bucket:         s3Bucket,
 		s3Region:         s3Region,
 		s3CfDistribution: s3CfDistribution,
+		s3Client:	  s3Client,
 		port:             port,
 	}
 
